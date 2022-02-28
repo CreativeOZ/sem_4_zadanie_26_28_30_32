c4ca391f93f4298692c21fd9f3ba6588cef3f379
CreativeOZ <zagriaoleg@gmail.com>
2022-02-28 17:19:13 +0300

    Возведите число А в натуральную степень B используя цикл.

---
+++ zadanie_26/Program.cs
@@ -0,0 +1,14 @@
+﻿// Возведите число А в натуральную степень B используя цикл.
+
+Console.Clear();
+
+Console.Write("Ввести число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Ввести число B: ");
+int B = int.Parse(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= B; i++)
+{
+    result = result * A;
+}
+Console.WriteLine("Число А в степени В = " + result);
