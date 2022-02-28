1d8cb1addb0695b3fe4c8cff03ca41a608b57483
CreativeOZ <zagriaoleg@gmail.com>
2022-02-28 20:38:50 +0300

    Покажите кубы чисел, заканчивающихся на чётную цифру.

---
+++ zadanie_30/Program.cs
@@ -0,0 +1,16 @@
+﻿// Покажите кубы чисел, заканчивающихся на чётную цифру.
+Console.Clear();
+
+Console.Write("Ввести число: ");
+int num = int.Parse(Console.ReadLine());
+int x = 2;
+while (x <= num)
+{
+    if (x % 2 == 0)
+    {
+        Console.WriteLine("Куб чисела заканчивающийся на чётную цифру: " + x * x * x);
+        {
+            x = x + 2;
+        }
+    }
+}
